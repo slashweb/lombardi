9145b2b0a68ae51875fb8b7158b708e5d647974c
GuillermoPC <memopc15@hotmail.com>
2022-11-26 08:52:29 -0700

    Funcion de prueba primeri commit

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -10,6 +10,12 @@ class HelloNear {
     return this.message;
   }
 
+  @view({}) // This method is read-only and can be called for free
+  get_my_products(): number[] {
+    return [1,2,3,4,5,6]; 
+  }
+
+
   @call({}) // This method changes the state, for which it cost gas
   set_greeting({ message }: { message: string }): void {
     near.log(`Saving greeting ${message}`);
